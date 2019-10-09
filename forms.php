2e5d4077e1d4ee75ed778a3e531d60be14175b96
Jeason riguelle <ridjea@gmail.com>
2019-10-09 11:29:22 +0200

    import de boostrap + semantique + css

---
+++ forms.php
@@ -0,0 +1,43 @@
+<?php
+
+
+
+
+?>
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<link rel="stylesheet" href="assets/style.css"/>
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Contact us</title>
+  </head>
+  <body class="container-fluid">
+    <section id="nav-bar" class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+    	<p>nav-bar</p>
+    </section>
+	<section id="forms" class="container-fluid col-sm-12 col-md-12 col-lg-12 col-xl-12">
+		<div id="formsOne" class="col-sm-12 col-md-6 col-lg-4">
+			<p>form1</p>
+		</div>
+		<div id="formsTwo" class="container-fluid col-sm-12 col-md-12 col-lg-12 col-xl-12">
+			<p>form2</p>
+		</div>
+		<div id="textarea" class="col-sm-12 col-md-12 col-lg-12 col-xl-12">
+			<p>textarea</p<
+		</div>
+		
+	</section>
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
