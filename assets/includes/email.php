80fd3353dae2dde689b00dba7dc353f7edaaf7f3
Jeason riguelle <ridjea@gmail.com>
2019-10-13 19:03:43 +0200

    organisation des fichier + favicon correction

---
+++ assets/includes/email.php
@@ -0,0 +1,47 @@
+<?php
+
+require __DIR__."/vendor/autoload.php";
+
+session_start();
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\SMTP;
+
+// Instantiation and passing `true` enables exceptions
+$mail = new PHPMailer(true);
+
+try {
+    //Server settings
+    $mail->SMTPDebug = false;                      // Enable verbose debug output
+    $mail->isSMTP();                                            // Send using SMTP
+    $mail->Host       = 'smtp-relay.sendinblue.com';                    // Set the SMTP server to send through
+    $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
+    $mail->Username   = 'ridjea@gmail.com';                     // SMTP username
+    $mail->Password   = '6ADTXz3SvNtcIZHF';                               // SMTP password
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
+    $mail->Port       = 587;                                    // TCP port to connect to
+
+    //Recipients
+    $mail->setFrom('ridjea@gmail.com', 'Jeason Riguelle');
+    $mail->addAddress($_SESSION["key"]["email"],  $_SESSION["key"]["name"]."".$_SESSION["key"]["family-name"]);     // Add a recipient
+
+    $message = "Bonjour" . " " . $_SESSION["key"]["gender"] ." " . $_SESSION["key"]["name"] . " " .$_SESSION["key"]["family-name"] . " " . " merci de rejoindre notre communautée";
+		$subject = "Formulaire";
+		$recip = $_SESSION["key"]['email'];
+
+    // Content
+    $mail->isHTML(true);                                  // Set email format to HTML
+    $mail->Subject = $subject;
+    $mail->Body    = $message;
+    $mail->AltBody = $message;
+
+    $mail->send();
+     $message;
+} catch (Exception $e) {
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+}
+	 
+		
+?>
+
--- index.php
+++ index.php
@@ -9,11 +9,11 @@ include "assets/includes/valided.php";
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<meta name="exercise" content="Author : Riguelle jeason " />
-	<link rel"shortcut icon" href="assets/image/hackers-poulette-logo.png"/>
+	<meta name="exercise" content="Author : Riguelle jeason " />	
 	<link rel="stylesheet" href="assets/css/style.css"/>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<link rel="icon" type="image/x-icon" href="assets/image/hackers-poulette-logo.png" />
 
     <title>Contact us</title>
   </head>
